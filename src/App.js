a4b6f1e2f8a1030b602876a83be92e69758f92fb
manibarathi <mani-barathi@users.noreply.github.com>
2022-05-29 17:05:14 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,41 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./styles/App.css";
-
 import Person from "./components/Person";
 import InputForm from "./components/InputForm";
 
 function App() {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState(() => {
+    console.log("retreiving from local storage");
+    const defaultValue = localStorage.getItem("persons");
+    return defaultValue ? JSON.parse(defaultValue) : [];
+  });
+
+  // component get executed/recreate
+  // html content is populated in screen
+  // then useEffect is exeuted
+  useEffect(() => {
+    console.log("setting value to localstorage");
+    localStorage.setItem("persons", JSON.stringify(persons));
+  }, [persons]);
 
   function appendPerson(newPerson) {
-    // persons.push(newPerson)  // wrong
-
-    // 1st way
-    // setPersons([...persons, newPerson]);
-
-    // 2nd way
     setPersons((prev) => {
-      console.log("prev", prev);
       newPerson.isAdult = newPerson.age >= 18 ? "Adult" : "child";
       return [...prev, newPerson];
     });
   }
 
-  // const arr = [1,2,3]
-  // const newarr = [...arr,...ar2,4,5]
+  function deletePerson(personName) {
+    setPersons((prev) => {
+      return prev.filter((person) => person.name !== personName);
+    });
+  }
 
-  // jsx
   return (
     <div className="App">
       <h1 className="text-teal"> Hello world</h1>
 
       <InputForm appendPerson={appendPerson} />
+
       {persons.map((person, index) => (
         <Person
           name={person.name}
           age={person.age}
           isAdult={person.isAdult}
           key={Math.random()}
+          deletePerson={deletePerson}
         />
       ))}
     </div>
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,9 +1,16 @@
-function Person({ name, age, isAdult }) {
+function Person({ name, age, isAdult, deletePerson }) {
+  const handleClick = () => {
+    deletePerson(name);
+  };
+
   return (
     <div className="flex">
       <p>{name} </p>
       <p>{age}</p>
       <p>{isAdult}</p>
+      <button type="button" onClick={handleClick}>
+        Delete
+      </button>
     </div>
   );
 }
